c9825ca4821667f7a99249e62e82cb604bddb619
Jacob Jewell <jacobjewell@eflexsystems.com>
2014-08-28 13:58:36 -0400

    first commit

---
+++ example.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var fs          = require('fs');
+var SambaClient = require('./');
+
+var testFile = 'test.txt';
+
+fs.writeFileSync(testFile, testFile);
+
+var client = new SambaClient({
+  address: process.argv[2]
+});
+
+client.sendFile(testFile, testFile, function(err) {
+  if (err) {
+    return console.error(err);
+  }
+
+  console.log('sent test file to samba share at ' + client.address);
+
+  fs.unlinkSync(testFile);
+
+  client.getFile(testFile, testFile, function(err) {
+    if (err) {
+      return console.error(err);
+    }
+
+    console.log('got test file from samba share at ' + client.address);
+  });
+});
+
+process.on('exit', function() {
+  fs.unlinkSync(testFile);
+});
---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var execFile = require('child_process').execFile;
+var util     = require('util');
+var p        = require('path');
+
+function SambaClient(options) {
+  this.address  = options.address;
+  this.username = options.username || 'guest';
+  this.password = options.password;
+}
+
+SambaClient.prototype.getFile = function(path, destination, cb) {
+  this.runCommand('get', path, destination, cb);
+};
+
+SambaClient.prototype.sendFile = function(path, destination, cb) {
+  this.runCommand('put', path, destination, cb);
+};
+
+SambaClient.prototype.runCommand = function(cmd, path, destination, cb) {
+  var passwordFlag = this.password ? this.password : '-N';
+  var workingDir   = p.dirname(path);
+  var fileName     = p.basename(path).replace('/', '\\');
+  var escapedDest  = destination.replace('/', '\\');
+  var fullCmd      = util.format('%s %s %s', cmd, fileName, escapedDest);
+
+  var args = ['-U', this.username, passwordFlag, '-c', fullCmd, this.address];
+
+  var options = {
+    cwd: workingDir
+  };
+
+  execFile('smbclient', args, options, function(err, stdout, stderr) {
+    // Samba is crazy and writes to standard error when the command is successful
+    var allOutput = stdout + stderr;
+
+    if (err) {
+      cb(err);
+    } else if (allOutput.indexOf('error') > -1 || allOutput.indexOf('fail') > -1) {
+      cb(new Error('Samba Error - ' + allOutput));
+    } else {
+      cb(null, allOutput);
+    }
+  });
+};
+
+module.exports = SambaClient;

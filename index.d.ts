79f99a57b6a8c0cfc6ce93c4a36052ae0458f620
J-Cat <joe@cataford.com>
2021-10-18 13:42:22 -0400

    Added typings and support for timeout value.

---
+++ index.d.ts
@@ -0,0 +1,36 @@
+declare module 'samba-client' {
+  interface ISambaClientOptions {
+    readonly address: string;
+    readonly username?: string;
+    readonly password?: string;
+    readonly domain?: string;
+    readonly port?: number;
+    readonly timeout?: number;
+    readonly maxProtocol?: string;
+    readonly maskCmd?: boolean;
+  }
+
+  interface ISambaItem {
+    readonly name: string;
+    readonly type: string;
+    readonly size: number;
+    readonly modifyTime: Date;
+  }
+
+  class SambaClient {
+    constructor(options: ISambaClientOptions);
+
+    getFile: (path: string, destination: string, workingDir?: string) => Promise<string | Buffer>;
+    sendFile: (path: string, destination: string) => Promise<string | Buffer>;
+    deleteFile: (fileName: string) => Promise<string | Buffer>;
+    listFiles: (fileNamePrefix: string, fileNameSuffix: string) => Promise<string[]>;
+    mkdir: (remotePath: string, cwd: string) => Promise<string | Buffer>;
+    dir: (remotePath: string, cwd: string) => Promise<string | Buffer>;
+    fileExists: (remotePath: string, cwd: string) => Promise<boolean>;
+    cwd: () => Promise<string>;
+    list: (remotePath: string) => Promise<ISambaItem[]>;
+  }
+
+  export = SambaClient;
+}
+
